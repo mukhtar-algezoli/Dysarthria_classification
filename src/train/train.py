362cd1de36e6dd924e98548ae1928ac1d9137bf6
mukhtar-algezoli <mukhtaralgezoli@gmail.com>
2024-03-24 12:58:50 +0400

    debugging accuracy issue

--- src/train/train.py
+++ src/train/train.py
@@ -25,13 +25,8 @@ def train_loop(dataloader, model, loss_fn, optimizer, device, wandb=None):
       optimizer.zero_grad()
 
       train_loss += loss_fn(pred.float(), batch_labels.unsqueeze(1).float().to(device)).item()
-      correct += (pred.round() == batch_labels.to(device)).type(torch.float).sum().item()
+      correct += (pred.round().squeeze() == batch_labels.to(device)).type(torch.float).sum().item()
     #   print(pred.round() == batch_labels.to(device))
-      print("//////////")
-      print(pred.round().squeeze())
-      print("/\/\//\//\/\//\/")
-      print(batch_labels)
-      print(f"pred len: {len(pred)}, labels len: {len(batch_labels)}")
       print(correct)
 
       if batch % 5 == 0:
