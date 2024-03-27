378b2ae74324a03bf992deb5dc5739a7b965b858
mukhtar-algezoli <mukhtaralgezoli@gmail.com>
2024-03-27 10:17:29 +0400

    reverting back to binary without augmentation

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -24,15 +24,7 @@ class UASpeechDataset(Dataset):
         return len(self.metadata)
 
     def __getitem__(self, idx):
-        # comment when working on binary data without augmentation
-        curr_audio_file_path = self.metadata.loc[self.metadata.index[idx], 'path']
-        curr_audio_file_path = curr_audio_file_path.replace("/l/users/rzan.alhaddad/noisereduce", "/UASpeech/audio/noisereduce")
-        if self.metadata.loc[self.metadata.index[idx], 'split'] != "B2":
-            curr_audio_file_path = curr_audio_file_path.replace("noisereduce", "noisereduce_aug")
-        audio_path = "./"+self.raw_data_dir +  curr_audio_file_path
-
-        #uncomment when working on binary data without augmentation
-        # audio_path = "./"+self.raw_data_dir +  self.metadata.loc[self.metadata.index[idx], 'path']
+        audio_path = "./"+self.raw_data_dir +  self.metadata.loc[self.metadata.index[idx], 'path']
         speech, samplerate = sf.read(audio_path)
         # seconds = librosa.get_duration(path= audio_path)
         # print(seconds)
@@ -47,10 +39,7 @@ class UASpeechDataset(Dataset):
 
 def get_train_test_val_set(args):
     # Load the data
-    # comment when working on binary data without augmentation
-    train_set = UASpeechDataset(os.path.join(args.data_splits_path, "train_df_binary_aug.csv"), args.data_path, args.SSL_model)
-    # uncomment when working on binary data without augmentation
-    # train_set = UASpeechDataset(os.path.join(args.data_splits_path, "train_df_binary.csv"), args.data_path, args.SSL_model)
+    train_set = UASpeechDataset(os.path.join(args.data_splits_path, "train_df_binary.csv"), args.data_path, args.SSL_model)
     test_set = UASpeechDataset(os.path.join(args.data_splits_path, "test_df_binary.csv"), args.data_path, args.SSL_model)
     val_set = UASpeechDataset(os.path.join(args.data_splits_path, "val_df_binary.csv"), args.data_path, args.SSL_model)
 
@@ -60,5 +49,4 @@ def get_train_test_val_set(args):
 
 
     return train_dataloader, test_dataloader, val_dataloader
-
 
